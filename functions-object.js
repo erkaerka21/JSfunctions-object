c6904b1a5c9d27d932888b20c031ca53ec3bdf70
erkaerka21 <eegiibaagii597@gmail.com>
2024-06-17 12:43:09 +0800

    6.17 JS functions and object

---
+++ functions-object.js
@@ -0,0 +1,55 @@
+// let a = 12;
+// let b = 16;
+// function hevle() {
+//   console.log("2 toonii niilber : ", a + b);
+// }
+// console.log("Functions");
+// hevle();
+
+// function hevle2(x1, x2) {
+//   console.log("2 тооны нийлбэр", x1 + x2);
+// }
+
+// hevle2(2, 6);
+// hevle2(78, 6);
+
+// function nemeh2(X, Y) {
+//   return X + Y;
+// }
+// let sumReturn = nemeh2(16, 39);
+// console.log("2 тооны нийлбэр буцаадаг : ", sumReturn);
+
+// //daalgavar2
+// function plus(a, b) {
+//   return a + b;
+// }
+// let plusReturn = plus(16, 29);
+// function negative(a, b) {
+//   return a - b;
+// }
+// let negativeReturn = plus(31, 19);
+// function urjver(a, b) {
+//   return a * b;
+// }
+// let urjverReturn = plus(16, 8);
+// function noogdvor(a, b) {
+//   return a / b;
+// }
+// let noogdvorReturn = plus(76, 38);
+
+// //хувьсагчид функц өгөх
+// let nemekh = function (a, b) {
+//   //anonymous function
+//   return a + b;
+// };
+// nemekh();
+
+//daalgavar1
+function compare(x, y) {
+  if (x > y) {
+    console.log("их тоо : ", x);
+  } else if (y > x) {
+    console.log("их тоо : ", y);
+  }
+  return;
+}
